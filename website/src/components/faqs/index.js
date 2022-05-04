b94ea7662a08089736bf0bd4d57b78d422a4db78
Grant Winship <grantwinship@hey.com>
2022-05-04 11:09:55 -0500

    Creates top level Guides section and tucks FAQs into Docs for phase out

--- website/src/components/faqs/index.js
+++ website/src/components/faqs/index.js
@@ -1,31 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 
-function FAQ({children, src, alt_header=null}) {
-  const file = require('../../../docs/faqs/' + src + '.md')
-  const meta = file.metadata;
-  const contents = file.default({});
+function FAQ({ children, src, alt_header = null }) {
+    const file = require('../../../docs/docs/faqs/' + src + '.md')
+    const meta = file.metadata;
+    const contents = file.default({});
 
-  const [isOn, setOn] = useState(false);
-  const toggleOn = function() {
-      setOn(!isOn);
-  }
+    const [isOn, setOn] = useState(false);
+    const toggleOn = function () {
+        setOn(!isOn);
+    }
 
-  return (
-      <div>
-          <span className={styles.link} onClick={toggleOn}>
-              <span className={styles.toggle}
+    return (
+        <div>
+            <span className={styles.link} onClick={toggleOn}>
+                <span className={styles.toggle}
                     style={{
                         transform: isOn ? null : 'rotateX(180deg)'
                     }}>
-              </span>&nbsp;
-              <span>{ alt_header || meta.title }</span>
-          </span>
-          <div style={{display: (isOn ? 'block' : 'none')}} className={styles.body}>
-              { contents }
-          </div>
-      </div>
-  );
+                </span>&nbsp;
+                <span>{alt_header || meta.title}</span>
+            </span>
+            <div style={{ display: (isOn ? 'block' : 'none') }} className={styles.body}>
+                {contents}
+            </div>
+        </div>
+    );
 }
 
 export default FAQ;
